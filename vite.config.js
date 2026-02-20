2e54c38671ed8936ff1deb378ac1d5a25a471c43
kindtiger <seonbeom2@gmail.com>
2026-02-20 14:55:26 +0900

    add. en

--- vite.config.js
+++ vite.config.js
@@ -28,6 +28,10 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "src/pages/index.html"), // 루트 진입점
         en: resolve(__dirname, "src/pages/en/index.html"),
+        en_about: resolve(__dirname, "src/pages/en/about.html"),
+        en_contact: resolve(__dirname, "src/pages/en/contact.html"),
+        en_portfolio: resolve(__dirname, "src/pages/en/portfolio.html"),
+        en_thanks: resolve(__dirname, "src/pages/en/thanks.html"),
         ko: resolve(__dirname, "src/pages/ko/index.html"),
         ko_about: resolve(__dirname, "src/pages/ko/about.html"),
         ko_contact: resolve(__dirname, "src/pages/ko/contact.html"),
